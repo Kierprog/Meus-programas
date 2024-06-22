62de25399c6501a75c89352bb667dad2cee6a9e4
Kierprog <kier14724@gmail.com>
2024-06-22 10:25:46 -0300

    DESAFIO 014

---
+++ DASAFIO 014.py
@@ -0,0 +1,4 @@
+# Programa que converte a temperatura em graus celsius para Fahrenheit
+
+c = float(input('informe a temperatura em ºC :'))
+print(f'a temperatura {c}ºC corresponde a {c * 1.8 + 32}F!')
